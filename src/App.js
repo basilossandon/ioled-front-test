5904af1b23eb691874827947336d3ea3a483b398
Basil Ossandon <basil.ossandon@usach.cl>
2019-12-13 23:29:17 -0300

    v0.0.1 Init React SPA

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'typeface-roboto';
+
+
+class App extends Component {
+  render() {
+    return (
+        <div className="body">
+          <h1> Welcome to iOLED </h1>
+        </div>
+    );
+  }
+}
+
+export default App;
